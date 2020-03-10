402175994a8a0025c6d9f9960286f64387de09f2
maryamnticha <maryam.nticha@gmail.com>
2020-03-10 15:19:11 +0100

    commit

---
+++ index.js
@@ -0,0 +1,24 @@
+express = require("express");
+const app = express();
+
+let hour = new Date().getHours();
+// let hour = 7;
+
+app.get("/", function(req, res) {
+  if (hour < 8 || hour > 17) res.sendFile(__dirname + "/public/out.html");
+  else res.sendFile(__dirname + "/public/home.html");
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(__dirname + "/public/contact.html");
+});
+app.get("/ourservices", (req, res) => {
+  res.sendFile(__dirname + "/public/ourservices.html");
+});
+app.get("/home", (req, res) => {
+  res.sendFile(__dirname + "/public/home.html");
+});
+app.listen(3000, err => {
+  if (err) console.log("server is not running");
+  else console.log("serveur is running on port 3000");
+});
